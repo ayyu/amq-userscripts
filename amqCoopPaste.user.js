5e26fdf0f4c89a774b3d9e7c813206bb03d1c6d8
Andrew Yu <ayyu@uwaterloo.ca>
2020-07-14 21:28:07 -0400

    Changed to underscore's debounce implementation

--- amqCoopPaste.user.js
+++ amqCoopPaste.user.js
@@ -13,58 +13,60 @@
 
 if (!window.setupDocumentDone) return;
 
+let minInterval = 500;
+
 let coopWindow;
 
 function setup() {
 	// create paster window
-    coopWindow = new AMQWindow({
-        title: "Co-op Autopaste",
-        width: 300,
-        height: 130,
-        zIndex: 1054,
-        draggable: true
-    });
-    coopWindow.addPanel({
-        width: 1.0,
-        height: 50,
-        id: "coopWindowControls"
-    });
+	coopWindow = new AMQWindow({
+		title: "Co-op Autopaste",
+		width: 300,
+		height: 130,
+		zIndex: 1054,
+		draggable: true
+	});
+	coopWindow.addPanel({
+		width: 1.0,
+		height: 50,
+		id: "coopWindowControls"
+	});
 	coopWindow.panels[0].panel.append($(`<div></div>`)
-        .addClass("customCheckboxContainer")
-        .append($(`<div></div>`)
-        	.addClass("customCheckbox")
-	        .append($(`<input id="coopPasteCheckbox" type="checkbox">`))
-	        .append($("<label for='coopPasteCheckbox'><i class='fa fa-check' aria-hidden='true'></i></label>"))
-        )
-        .append($(`<label for="coopPasteCheckbox"></label>`)
-        	.addClass("customCheckboxContainerLabel")
-        	.text("Enable auto-paste"))
-    );
+		.addClass("customCheckboxContainer")
+		.append($(`<div></div>`)
+			.addClass("customCheckbox")
+			.append($(`<input id="coopPasteCheckbox" type="checkbox">`))
+			.append($("<label for='coopPasteCheckbox'><i class='fa fa-check' aria-hidden='true'></i></label>"))
+		)
+		.append($(`<label for="coopPasteCheckbox"></label>`)
+			.addClass("customCheckboxContainerLabel")
+			.text("Enable auto-paste"))
+	);
 
 	// Adds button to in-game options to enable paster
-    let oldWidth = $("#qpOptionContainer").width();
-    $("#qpOptionContainer").width(oldWidth + 35);
-    $("#qpOptionContainer > div").append($(`<div id="qpCoopPaster" class="clickAble qpOption"><i aria-hidden="true" class="fa fa-paste qpMenuItem"></i></div>`)
-        .click(() => {
-            if (coopWindow.isVisible()) {
-                coopWindow.close();
-            }
-            else {
-                coopWindow.open();
-            }
-        })
-        .popover({
-            content: "Co-op Autopaster",
-            trigger: "hover",
-            placement: "bottom"
-        })
-    );
+	let oldWidth = $("#qpOptionContainer").width();
+	$("#qpOptionContainer").width(oldWidth + 35);
+	$("#qpOptionContainer > div").append($(`<div id="qpCoopPaster" class="clickAble qpOption"><i aria-hidden="true" class="fa fa-paste qpMenuItem"></i></div>`)
+		.click(() => {
+			if (coopWindow.isVisible()) {
+				coopWindow.close();
+			}
+			else {
+				coopWindow.open();
+			}
+		})
+		.popover({
+			content: "Co-op Autopaster",
+			trigger: "hover",
+			placement: "bottom"
+		})
+	);
 
-    // add Enter key listener for copypasta
+	// add Enter key listener for copypasta
 	let quizReadyListener = new Listener("quiz ready", data => {
 		if (quiz.gameMode !== "Ranked") {
 			$("#qpAnswerInput").off("keypress", answerHandler)
-            .on("keypress", answerHandler);
+			.on("keypress", answerHandler);
 		}
 		else {
 			$("#qpAnswerInput").off("keypress", answerHandler);
@@ -72,20 +74,32 @@ function setup() {
 	});
 
 	// Sends current answer as chat message
-	let answerHandler = function (event) {
+	let answerHandler = debounce( function (event) {
 		if (event.which === 13 && $("#coopPasteCheckbox").prop("checked")) {
 			gameChat.$chatInputField.val($("#qpAnswerInput").val());
 			gameChat.sendMessage();
-            $("#qpAnswerInput").off("keypress", answerHandler)
-            .delay(500)
-            .on("keypress", answerHandler);
 		}
-	}
+	}, minInterval, true);
 
 	quizReadyListener.bindListener();
 
-    console.log("co-op setup");
+	console.log("co-op setup");
 }
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
 setup();
 
@@ -94,31 +108,31 @@ AMQ_addScriptData({
 	author: "ayyu",
 	description: `
 		<p>Automatically pastes your submitted answer to chat, for grinding co-op lobbies. This script is disabled during ranked.</p>
-        <p>The script can be turned on by clicking on the clipboard icon in the top right next to the settings icon while in a quiz.</p>
-        <p>Piggybacks off of some parts by TheJoseph98</p>
+		<p>The script can be turned on by clicking on the clipboard icon in the top right next to the settings icon while in a quiz.</p>
+		<p>Piggybacks off of some parts by TheJoseph98</p>
 	`
 });
 
 AMQ_addStyle(`
 	#qpCoopPaster {
-        width: 30px;
-        margin-right: 5px;
-    }
-    #coopWindowControls .customCheckboxContainer {
-    	padding: 1rem 0;
+		width: 30px;
+		margin-right: 5px;
+	}
+	#coopWindowControls .customCheckboxContainer {
+		padding: 1rem 0;
 		align-items: center;
-	  	justify-content: center;
-    }
-    .customCheckboxContainer {
-        display: flex;
-    }
-    .customCheckboxContainer > div {
-        display: inline-block;
-        margin: 5px 0px;
-    }
-    .customCheckboxContainer > .customCheckboxContainerLabel {
-        margin-left: 5px;
-        margin-top: 5px;
-        font-weight: normal;
-    }
+		justify-content: center;
+	}
+	.customCheckboxContainer {
+		display: flex;
+	}
+	.customCheckboxContainer > div {
+		display: inline-block;
+		margin: 5px 0px;
+	}
+	.customCheckboxContainer > .customCheckboxContainerLabel {
+		margin-left: 5px;
+		margin-top: 5px;
+		font-weight: normal;
+	}
 `);
