75f6fb9e50f2c4e04a4a0a746a04c88e9da51527
ayyu <ayyu@uwaterloo.ca>
2021-04-02 15:00:02 -0400

    Added usage for auto volume

--- encoding/amq_volume_auto.sh
+++ encoding/amq_volume_auto.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
+usage () {
+	cat <<EOM
+Usage: $(basename $0) -i <dir>
+-i     input directory. defaults to "source"
+EOM
+	exit 1
+}
+
 in_dir="source"
 
-for file in $in_dir/*
+while getopts hi: flag
+do
+	case "${flag}" in
+		h) usage;;
+		i) in_dir=${OPTARG};;
+	esac
+done
+
+for file in "$in_dir"/*
 do
 	bash "${BASH_SOURCE%/*}/amq_volume_norm.sh" -i "$file"
 done
