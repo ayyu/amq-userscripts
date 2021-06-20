60bf9ff8a3632d36486ecc834a6c1ce67ed3d9fe
ayyu <ayyu@uwaterloo.ca>
2021-06-20 15:39:27 -0400

    added switch to lowercase

--- userscripts/amqLowercase.user.js
+++ userscripts/amqLowercase.user.js
@@ -7,31 +7,47 @@
 // @match        https://animemusicquiz.com/*
 // @grant        none
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js
-// @downloadURL  https://raw.githubusercontent.com/ayyu/amq-scripts/master/userscripts/amqCoopPaste.user.js
+// @downloadURL  https://raw.githubusercontent.com/ayyu/amq-scripts/master/userscripts/amqLowercase.user.js
 // ==/UserScript==
 
 if (document.getElementById('startPage')) {
 	return;
 }
 
+let lowercaseButton;
+let lowercaseSwitch = false;
+
 function quizJoinHandler(data) {
-	if (quiz.gameMode !== "Ranked") {
-		quiz.answerInput.$input.off("keypress", answerHandler)
-		.on("keypress", answerHandler);
-	}
-	else {
-		quiz.answerInput.$input.off("keypress", answerHandler);
-	}
+	quiz.answerInput.$input.off("keypress", answerHandler)
+	.on("keypress", answerHandler);
 }
 
 function answerHandler(event) {
 	var answer = quiz.answerInput.$input.val();
-	if (event.which === 13) {
+	if (event.which === 13 && lowercaseSwitch) {
 		quiz.answerInput.setNewAnswer(answer.toLowerCase());
 	}
 }
 
 function setup() {
+	lowercaseButton = $(`<div id="qpLowercaseButton" class="clickAble qpOption"><i aria-hidden="true" class="fa fa-wheelchair-alt qpMenuItem"></i></div>`);
+	lowercaseButton.popover({
+		placement: "bottom",
+		content: "Toggle co-op copy paste to chat",
+		trigger: "hover"
+	});
+	lowercaseButton.click(function () {
+		msg = (lowercaseSwitch ? "Disabled" : "Enabled") + " auto lowercase.";
+		gameChat.systemMessage(msg);
+		lowercaseSwitch = !lowercaseSwitch;
+		$(`#qpLowercaseButton i`).toggleClass('fa-inverse', lowercaseSwitch);
+	});
+
+	// Adds button to in-game options to enable paster
+	let oldWidth = $("#qpOptionContainer").width();
+	$("#qpOptionContainer").width(oldWidth + 35);
+	$("#qpOptionContainer > div").append(lowercaseButton);
+
 	// add Enter key listener for copypasta
 	new Listener("quiz ready", (data) => {
 		quizJoinHandler(data);
@@ -42,3 +58,11 @@ function setup() {
 }
 
 setup();
+
+AMQ_addStyle(`
+	#qpLowercaseButton {
+		width: 30px;
+		height: 100%;
+		margin-right: 5px;
+	}
+`);
