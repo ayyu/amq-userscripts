a45c3427f4d438c0b920e6d31fb6ff62aa407273
ayyu <ayyu@uwaterloo.ca>
2021-08-07 14:32:50 -0400

    split encoding scripts into separate repo

--- encoding/amq_settings.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-target_max=-1.0
-target_mean=-18.0
-meta_settings="-map_metadata -1 -map_chapters -1 -sn"
-audio_settings="-b:a 320k -ac 2"
-opus_settings="-c:a libopus $audio_settings"
-mp3_settings="-c:a libmp3lame $audio_settings"
--- encoding/sample_encode.sh
+++
@@ -1,9 +0,0 @@
-amq_encode.sh -i "source.mkv" -ss 15:24.883 -to 16:09.300
-amq_volume_auto.sh -i source
-
-# if you were muxing your video with clean audio instead of broadcast audio,
-# you would comment out the 2nd line "amq_volume_auto.sh -i source"
-# and use the following instead, assuming clean.wav is your clean audio file:
-
-# amq_mux.sh -a clean.wav -i source
-# amq_volume_auto.sh -i clean
